45c212de74d12aad890f5fe77928f259999d0bba
Huaxuan Zhang <koogle.zhang@gmail.com>
2020-02-13 00:49:28 +0800

    调整指标通知

--- app/models/indicator.rb
+++ app/models/indicator.rb
@@ -10,35 +10,11 @@
 
 class Indicator < ActiveRecord::Base
   belongs_to :market
-  after_save :step_buy_or_sell
+  after_save :dingding_notice
   scope :recent, -> { order('created_at desc') }
   self.per_page = 10
 
-  def step_buy_or_sell
-    if name.include? 'step_buy'
-      retain = market.regulate&.retain || 0
-      market.sync_fund
-      fund = market.fund&.balance || 0
-      if retain > 0 && fund < retain * 0.1
-        amount = retain - fund
-        market.step_price_bid(amount)
-      end
-    elsif name.include? 'step_sell'
-      retain = market.regulate&.retain || 0
-      market.sync_fund
-      fund = market.fund&.balance || 0
-      last_orders = market.bid_filled_orders.last(10)
-      bid_price = last_orders.map { |x| x['price'].to_f }.sum / last_orders.size
-      recent_price = market.recent_price
-      if fund > retain * 0.1 && retain > 0
-        amount = fund > amount ? amount : fund
-        if recent_price > bid_price
-          market.step_price_ask(amount)
-        elsif recent_price < bid_price
-          market.market_price_ask(amount)
-        end
-      end
-    end
+  def dingding_notice
+    Notice.dingding(name)
   end
-
 end
