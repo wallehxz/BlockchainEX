ef182e115498f26b48d3b2f1a22330b1dc0745e9
Huaxuan Zhang <koogle.zhang@gmail.com>
2020-08-02 10:24:30 +0800

    调整止损卖出策略为阶梯价

--- lib/daemons/guarant.rb
+++ lib/daemons/guarant.rb
@@ -24,10 +24,10 @@ while($running) do
         coin.sync_fund
         balance = coin.fund.balance
         if balance > 1
-          coin.market_price_ask(balance / 3)
+          coin.step_price_ask(balance / 4)
           Notice.sms("\n 开启止损操作：\n> 数量: #{balance / 3} \n> 价格: #{_latest} \n")
         else
-          coin.market_price_ask(balance * 0.998)
+          coin.step_price_ask(balance * 0.998)
           Notice.sms("\n 开启止损操作：\n> 数量: #{balance} \n> 价格: #{_latest} \n")
           regul.update(fast_trade: false)
         end
