3a106e5a27726a61a54fd90dff79b18ed1f66d18
WalleHXZ <kooglezhang@gmail.com>
2021-07-25 21:54:50 +0800

    24小时行情监控类别添加合约

--- lib/daemons/fluctuation.rb
+++ lib/daemons/fluctuation.rb
@@ -15,6 +15,7 @@ Signal.trap("TERM") do
 end
 
 def binance_usdt_tickers
+  tip = "【USDT现货行情】\n#{Time.now.to_s(:short)}\n"
   ticker_url = 'https://api.binance.com/api/v1/ticker/24hr'
   res = Faraday.get do |req|
     req.url ticker_url
@@ -29,17 +30,38 @@ def binance_usdt_tickers
   end
   usdt_marts = usdt_marts.select {|x| !x['symbol'].include?('UPUSDT')}
   usdt_marts = usdt_marts.select {|x| !x['symbol'].include?('DOWNUSDT')}
-  usdt_marts.sort {|x, y| y['priceChangePercent'].to_f <=> x['priceChangePercent'].to_f }
+  tickers = usdt_marts.sort {|x, y| y['priceChangePercent'].to_f <=> x['priceChangePercent'].to_f }
+  (tickers[0] + tickers[-1]).each do |quote|
+    tip << "#{quote['symbol']} 涨幅[#{quote['priceChangePercent']}],报价[#{quote['lastPrice'].to_f}]\n"
+  end
+  Notice.dingding(tip)
+end
+
+def future_usdt_tickers
+  tip = "【USDT合约行情】\n#{Time.now.to_s(:short)}\n"
+  ticker_url = 'https://fapi.binance.com/fapi/v1/ticker/24hr'
+  res = Faraday.get do |req|
+    req.url ticker_url
+  end
+  markets = JSON.parse(res.body)
+  usdt_marts = []
+  markets.each do |quote|
+    usdt_quot = (quote['symbol'] =~ /USDT/) || 0
+    if usdt_quot > 2
+      usdt_marts << quote
+    end
+  end
+  tickers = usdt_marts.sort {|x, y| y['priceChangePercent'].to_f <=> x['priceChangePercent'].to_f }
+  (tickers[0] + tickers[-1]).each do |quote|
+    tip << "#{quote['symbol']} 涨幅[#{quote['priceChangePercent']}],报价[#{quote['lastPrice'].to_f}]\n"
+  end
+  Notice.dingding(tip)
 end
 
 while($running) do
   begin
-    tip = ''
-    tickers = binance_usdt_tickers
-    (tickers[0..2] + tickers[-3..-1]).each do |quote|
-      tip << "#{quote['symbol']} 涨幅[#{quote['priceChangePercent']}],报价[#{quote['lastPrice'].to_f}]\n"
-    end
-    Notice.dingding(tip)
+    future_usdt_tickers
+    binance_usdt_tickers
   rescue => detail
     Notice.exception(detail, "Deamon Fluctuation")
   end
